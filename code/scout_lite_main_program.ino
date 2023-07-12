8940ab1de6a6ea21062b0602b78207b8296311d0
Haiden McGill <46386953+heyhaiden@users.noreply.github.com>
2023-07-12 17:27:20 +0100

    Refactored full program

--- code/scout_lite_main_program.ino
+++ code/scout_lite_main_program.ino
@@ -2,7 +2,7 @@
 *****************************
 FIREWALL: SCOUT LITE
 SOLAR POWERED DEVICE
-V3
+V4
 *****************************
 
 */
@@ -15,79 +15,79 @@ V3
 #include <Wire.h>
 #include <LoRa.h>
 
-// TTN Configuration
 //#include "ttn_config_secrets.h"
 //#include "ttn_config_secrets_2.h"
-//#include "ttn_config_secrets_3.h"
-
-String appEui = SECRET_APP_EUI;
-String appKey = SECRET_APP_KEY;
-
-LoRaModem modem;
-Adafruit_BME680 bme;
+#include "ttn_config_secrets_3.h"
 
 #define PGOOD_PIN 1
 #define CHG_PIN 2
-
-#define SAMPLES 10
+#define CHARGE_SAMPLES 10
 #define SAMPLE_DELAY 100
+#define ERROR_FLAG 255
+#define MAX_JOIN_ATTEMPTS 8
+#define JOIN_RETRY_SLEEP_MINUTES 30
+#define JOIN_RETRY_DELAY_MINUTES 2
+#define LORA_CONNECTION_STABILIZATION_DELAY_SECONDS 5
+#define ERROR_BLINK_COUNT 5
+
+uint32_t DUTY_CYCLE_MINUTES = 10;
 
 enum SolarStatus { OFF = 0, ACTIVE = 1 };
 enum BatteryStatus { CHARGING = 0, FULL = 1, DRAINING = 2 };
 
-SolarStatus solar_status;
-BatteryStatus battery_status;
+class LoRaWAN {
+public:
+  LoRaModem modem;
+  String appEui = SECRET_APP_EUI;
+  String appKey = SECRET_APP_KEY;
+  uint16_t packetCount = 0;
 
-uint16_t packetCount = 0;
-uint32_t DUTY_CYCLE = 10 * 60 * 1000;
+  void init();
+  void handleDownlink();
+  void sendData(byte* payload, size_t payloadSize);
+};
+
+class BME680Sensor {
+public:
+  Adafruit_BME680 bme;
+  void init();
+  void readAndSendData(LoRaWAN& lorawan);
+};
+
+class DeviceHealth {
+public:
+  SolarStatus solar_status;
+  BatteryStatus battery_status;
+
+  void update();
+};
+
+// Declare objects globally
+LoRaWAN lorawan;
+BME680Sensor sensor;
+DeviceHealth deviceHealth;
 
 void setup() {
-  
   Serial.begin(115200);
   delay(1000);
-  
+
   pinMode(PGOOD_PIN, INPUT);
   pinMode(CHG_PIN, INPUT);
 
   Wire.begin();
 
-  initLoRaWan();
-  initSensor();
-  
+  lorawan.init();
+  sensor.init();
 }
 
 void loop() {
-
-  readSensorAndSendData();
-
-  handleDownlink();
-
-  LowPower.sleep(DUTY_CYCLE);
-  
+  sensor.readAndSendData(lorawan);
+  lorawan.handleDownlink();
+  LowPower.sleep(DUTY_CYCLE_MINUTES * 60 * 1000);
 }
 
-void initSensor() {
-
-  if (!bme.begin(0x76)) {
-    Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1)
-      ;
-  }
-
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_8X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150);
-
-  Serial.println("BME680 sensor initialized and calibrated.");
-}
-
-void initLoRaWan() {
+void LoRaWAN::init() {
   int joinAttempts = 0;
-  int maxJoinAttempts = 4;
-  uint32_t sleepPeriod = 30 * 60 * 1000;  // sleep for 60 minutes, in milliseconds
-
   if (!modem.begin(EU868)) {
     Serial.println("Failed to start module");
     while (1) {}
@@ -100,44 +100,27 @@ void initLoRaWan() {
 
   int connected = modem.joinOTAA(appEui, appKey);
 
-  while (!connected && joinAttempts < maxJoinAttempts) {
+  while (!connected && joinAttempts < MAX_JOIN_ATTEMPTS) {
     joinAttempts++;
     Serial.println("Not connected, trying again...");
-    delay(10000);  // wait 10 seconds before next attempt
+    delay(JOIN_RETRY_DELAY_MINUTES * 60 * 1000);  // wait before next attempt
     connected = modem.joinOTAA(appEui, appKey);
   }
 
   if (connected) {
     Serial.println("Network joined!");
   } else {
-    Serial.println("Failed to join network after several attempts. The device is going into a low power mode for 1 hour before trying again.");
-    LowPower.sleep(sleepPeriod);  // sleep for 1 hour
-    initLoRaWan();                // try to initialize the LoRaWAN connection again
+    Serial.println("Failed to join network after several attempts. The device is going into a low power mode before trying again.");
+    LowPower.sleep(JOIN_RETRY_SLEEP_MINUTES * 60 * 1000);  // sleep
+    init();  // try to initialize the LoRaWAN connection again
   }
 
   modem.setADR(true);
   modem.minPollInterval(60);
-  delay(5000);  // wait 5 seconds to stabilize connection
+  delay(LORA_CONNECTION_STABILIZATION_DELAY_SECONDS * 1000);  // wait to stabilize connection
 }
 
-void readSensorAndSendData() {
-  unsigned long endTime = bme.beginReading();
-  if (endTime == 0) {
-    Serial.println("Failed to begin reading");
-    return;
-  }
-
-  updateDeviceHealth();
-
-  if (!bme.endReading()) {
-    Serial.println("Failed to complete reading");
-    return;
-  }
-
-  sendDataOverLoRaWAN();
-}
-
-void handleDownlink() {
+void LoRaWAN::handleDownlink() {
   String receivedData;
   int i = 0;
   while (modem.available()) {
@@ -155,49 +138,103 @@ void handleDownlink() {
   // If the received data is the "EXTRA_MEASURE" command, take an extra measurement
   if (receivedData == "EXTRA_MEASURE") {
     Serial.println("Extra measurement command received. Taking extra measurement...");
-    readSensorAndSendData();
+    BME680Sensor sensor;
+    sensor.readAndSendData(*this);
   } else {
     // If the received data is a number, use it to set the duty cycle
-    DUTY_CYCLE = receivedData.toInt() * 60 * 1000;  // convert minutes to milliseconds
+    for (char c : receivedData) {
+      if (!isDigit(c)) {
+        Serial.println("Invalid data received for duty cycle. Ignoring...");
+        return;
+      }
+    }
+    DUTY_CYCLE_MINUTES = receivedData.toInt();  
   }
 }
 
+void LoRaWAN::sendData(byte* payload, size_t payloadSize) {
+  int err;
+  modem.beginPacket();
+  modem.write(payload, payloadSize);
+  err = modem.endPacket(true);
 
-void printSensorReading() {
-  // Print the sensor readings to the console
-  Serial.println("***BME688***");
-
-  Serial.print("Temperature = ");
-  Serial.print(bme.temperature);
-  Serial.println(" *C");
-
-  Serial.print("Pressure = ");
-  Serial.print(bme.pressure / 100.0);
-  Serial.println(" hPa");
-
-  Serial.print("Humidity = ");
-  Serial.print(bme.humidity);
-  Serial.println(" %");
-
-  Serial.print("Gas Resistance = ");
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(" KOhms");
-
-  Serial.println();
+  if (err > 0) {
+    packetCount++;
+    Serial.println("LoRa Packet sent");
+  } else {
+    Serial.println("Error sending LoRa Packet");
+  }
 }
 
-void updateDeviceHealth() {
+void BME680Sensor::init() {
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    errorLed(ERROR_BLINK_COUNT);
+    LoRaWAN lorawan;
+    String errorMessage = "Error: Could not find a valid BME680 sensor.";
+    byte payload[errorMessage.length() + 1];
+    payload[0] = ERROR_FLAG;
+    memcpy(payload + 1, errorMessage.c_str(), errorMessage.length());
+    lorawan.sendData(payload, sizeof(payload));
+    LowPower.sleep(DUTY_CYCLE_MINUTES * 60 * 1000);
+  }
 
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_8X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150);
+
+  Serial.println("BME680 sensor initialized and calibrated.");
+}
+
+void BME680Sensor::readAndSendData(LoRaWAN& lorawan) {
+  unsigned long endTime = bme.beginReading();
+  if (endTime == 0) {
+    Serial.println("Failed to begin reading");
+    return;
+  }
+
+  DeviceHealth deviceHealth;
+  deviceHealth.update();
+
+  if (!bme.endReading()) {
+    Serial.println("Failed to complete reading");
+    return;
+  }
+
+  int temperature = bme.temperature * 100.0;
+  int humidity = bme.humidity * 100.0;
+  int pressure = 10 * (bme.pressure / 100.0);
+  int gas_resistance = bme.gas_resistance / 10.0;
+
+  byte payload[11];
+  payload[0] = highByte(temperature);
+  payload[1] = lowByte(temperature);
+  payload[2] = highByte(humidity);
+  payload[3] = lowByte(humidity);
+  payload[4] = highByte(pressure);
+  payload[5] = lowByte(pressure);
+  payload[6] = highByte(gas_resistance);
+  payload[7] = lowByte(gas_resistance);
+  payload[8] = (deviceHealth.battery_status << 2) | deviceHealth.solar_status;
+  payload[9] = highByte(lorawan.packetCount);
+  payload[10] = lowByte(lorawan.packetCount);
+
+  lorawan.sendData(payload, sizeof(payload));
+}
+
+void DeviceHealth::update() {
   int pgood_count = 0;
   int chg_count = 0;
 
-  for (int i = 0; i < SAMPLES; i++) {
+  for (int i = 0; i < CHARGE_SAMPLES; i++) {
     pgood_count += digitalRead(PGOOD_PIN) == LOW ? 1 : 0;
     chg_count += digitalRead(CHG_PIN) == LOW ? 1 : 0;
     delay(SAMPLE_DELAY);
   }
 
-  if (pgood_count > SAMPLES / 2) {
+  if (pgood_count > CHARGE_SAMPLES / 2) {
     solar_status = ACTIVE;
     Serial.println("Solar panel is ACTIVE.");
   } else {
@@ -205,11 +242,9 @@ void updateDeviceHealth() {
     Serial.println("Solar panel is OFF.");
   }
 
-  if (chg_count > SAMPLES / 2) {
+  if (chg_count > CHARGE_SAMPLES / 2) {
     battery_status = CHARGING;
     Serial.println("Battery is CHARGING.");
-    // If the battery is charging, but the solar panel is reported as off, 
-    // then the solar panel must actually be on. 
     if (solar_status == OFF) {
       solar_status = ACTIVE;
       Serial.println("Correction: Solar panel is ACTIVE.");
@@ -226,66 +261,12 @@ void updateDeviceHealth() {
   Serial.println();
 }
 
-void sendDataOverLoRaWAN() {
-  int temperature = bme.temperature * 100.0;
-  int humidity = bme.humidity * 100.0;
-  int pressure = 10 * (bme.pressure / 100.0);
-  int gas_resistance = bme.gas_resistance / 10.0;
-
-  byte payload[11];
-  payload[0] = highByte(temperature);
-  payload[1] = lowByte(temperature);
-  payload[2] = highByte(humidity);
-  payload[3] = lowByte(humidity);
-  payload[4] = highByte(pressure);
-  payload[5] = lowByte(pressure);
-  payload[6] = highByte(gas_resistance);
-  payload[7] = lowByte(gas_resistance);
-  payload[8] = (battery_status << 2) | solar_status;
-  payload[9] = highByte(packetCount);
-  payload[10] = lowByte(packetCount);
-
-  int err;
-  modem.beginPacket();
-  modem.write(payload, sizeof(payload));
-  err = modem.endPacket(true);
-
-  if (err > 0) {
-    packetCount++;
-    Serial.println("LoRa Packet sent");
-  } else {
-    Serial.println("Error sending LoRa Packet");
+void errorLed(int blinkCount) {
+  pinMode(LED_BUILTIN, OUTPUT);
+  for (int i = 0; i < blinkCount; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
   }
-}
-
-void errLeds(void) {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
-}
-
-void shortLeds(void) {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
 }
